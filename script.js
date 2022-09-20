5efe67832469ce0becaa0c31be3bf45f0a3c4ab8
Bain Schmidt <Bainschmidt@gmail.com>
2022-09-20 19:05:48 -0400

    Likes

---
+++ script.js
@@ -0,0 +1,26 @@
+var count1 = 0;
+var countElement1 = document.querySelector("#count1");
+
+function add1() {
+    count1++;
+    countElement1.innerText = count1 + " Like(s)"
+    console.log(count1);
+}
+
+var count2 = 0;
+var countElement2 = document.querySelector("#count2");
+
+function add2() {
+    count2++;
+    countElement2.innerText = count2 + " Like(s)"
+    console.log(count2);
+}
+
+var count3 = 0;
+var countElement3 = document.querySelector("#count3");
+
+function add3() {
+    count3++;
+    countElement3.innerText = count3 + " Like(s)"
+    console.log(count3);
+}
